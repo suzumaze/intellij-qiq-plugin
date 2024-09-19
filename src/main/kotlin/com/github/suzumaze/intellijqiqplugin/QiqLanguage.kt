490a258f58b536e9c5af5337a4afdb5158d5e51d
Kenichi Horikawa <horikawa@bitnauts.co.jp>
2024-09-19 14:43:47 +0900

    implementation TemplateLanguage and InjectableLanguage

--- src/main/kotlin/com/github/suzumaze/intellijqiqplugin/QiqLanguage.kt
+++ src/main/kotlin/com/github/suzumaze/intellijqiqplugin/QiqLanguage.kt
@@ -1,9 +1,11 @@
 package com.github.suzumaze.intellijqiqplugin
 
+import com.intellij.lang.InjectableLanguage
 import com.intellij.lang.Language
+import com.intellij.psi.templateLanguages.TemplateLanguage
 import com.jetbrains.php.lang.PhpLanguage
 
-object QiqLanguage : Language(PhpLanguage.INSTANCE, "Qiq") {
+object QiqLanguage : Language(PhpLanguage.INSTANCE, "Qiq"), TemplateLanguage, InjectableLanguage {
     private fun readResolve(): Any = QiqLanguage
     val INSTANCE: QiqLanguage = QiqLanguage
 }
